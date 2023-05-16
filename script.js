6eb69967ecb05c2c32f3386eb2693206a6d24591
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-16 23:40:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots.sort((a,b) => {
+	  let nameA = a.toLowerCase().substring(2); // ignorer upperCase
+	let nameB = b.toLowerCase().substring(2);
+	if(nameA > nameB){
+		return 1;
+	}
+	if(nameA < nameB)
+	{
+		return -1;
+	}
+	return 0;
+})
+const ul = document.getElementById('bands');
+touristSpots.forEach((bandName) => {
+  const li = document.createElement('li');
+  li.textContent = bandName;
+  ul.appendChild(li);
+});
