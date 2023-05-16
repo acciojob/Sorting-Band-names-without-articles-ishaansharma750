a0dc5cf04bb52ef39dec85cf5247ad20250fe07a
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-16 23:42:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+
+//your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort((a,b) => {
-	  let nameA = a.toLowerCase().substring(2); // ignorer upperCase
-	let nameB = b.toLowerCase().substring(2);
-	if(nameA > nameB){
-		return 1;
-	}
-	if(nameA < nameB)
-	{
-		return -1;
-	}
-	return 0;
+touristSpots.sort((a,b)=>{
+    let nameA = a.toLowerCase().substring(2); // ignorer upperCase
+    let nameB = b.toLowerCase().substring(2); // ignorer upperCase
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+    return 1;
+    }
+
+    // names must be equal
+    return 0;
 })
+// Generate the list items
 const ul = document.getElementById('bands');
 touristSpots.forEach((bandName) => {
   const li = document.createElement('li');
